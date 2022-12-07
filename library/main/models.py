b52f6206aa598e76f4d3eb50650d605b9542a425
abdulhaq-007 <85011586+abdulhaq-007@users.noreply.github.com>
2022-12-07 14:29:40 -0800

    add

--- library/main/models.py
+++ library/main/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100)
+
+    def __str__(self):
+        return str(self.name)
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.SlugField(max_length=100)
+    country = models.CharField( max_length=50)
+
+    def __str__(self):
+        return str(self.name) 
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.name)
+
