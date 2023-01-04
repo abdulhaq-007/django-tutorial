5fc4a35d47e9cfc43a4c06aa32c8e6638a4b825c
abdulhaq-007 <85011586+abdulhaq-007@users.noreply.github.com>
2023-01-04 15:43:56 -0800

    add

--- pyforum/accounts/views.py
+++ pyforum/accounts/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView
+from .models import User
 # Create your views here.
+class ProfileView(TemplateView):
+    template_name = 'registration/profile.html'
