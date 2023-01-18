f38bdeef23c5241aa376df52af5891bfcb69747b
abdulhaq-007 <85011586+abdulhaq-007@users.noreply.github.com>
2023-01-18 15:53:52 -0800

    update

--- laptopshop/shop/views.py
+++ laptopshop/shop/views.py
@@ -1,5 +1,18 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic.base import View
 # Create your views here.
-class HopePageView(TemplateView):
+from .models import Product
+
+class HopePageView(View):
     template_name = 'index.html'
+
+    def get(self, request, *args, **kwargs):
+        popular = Product.objects.filter(top=True)
+        discount = Product.objects.filter(discount__gte=0)
+
+        data = {
+            "popular_products":popular,
+            "discount_products":discount
+        }
+        return render(request, self.template_name)
+
